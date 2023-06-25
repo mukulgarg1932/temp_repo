4d837ef6bbb4c4522f1676940cceff47332465c3
mukulgarg1932 <mukulgarg1932@gmail.com>
2023-06-25 12:13:43 +0530

    three files to be add

---
+++ nosql_db.py
@@ -0,0 +1,14 @@
+
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://mukulg705:<mukulg705>@my-cluser01.1sp4kzw.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
---
+++ temp.py
@@ -0,0 +1,2 @@
+num = int(input("Please Enter your number - "))
+print(f"You entered number -- {num}")
